14ed673e84ab2069e1c47df4853304168469cb77
Sofia Anisimova <sofiaanisimova@yandex.by>
2019-11-01 20:43:30 +0300

    Revert "Revert "first commit""
    This reverts commit b01647f8015b6ce5d5285791e028dc836644e850.

---
+++ Admin's_main.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FatCat
+{
+    /// <summary>
+    /// Логика взаимодействия для Admin_s_main.xaml
+    /// </summary>
+    public partial class Admin_s_main : Window
+    {
+        public Admin_s_main()
+        {
+            InitializeComponent();
+        }
+    }
+}
